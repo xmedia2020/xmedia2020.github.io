073731ea0813bc2f59ac9fe3ff365b584c9aae25
mattdella <37800031+mattdella22@users.noreply.github.com>
2020-04-07 13:58:18 +0200

    Aggiunto lettere Y e o

--- abc/MDE/e/main.js
+++ abc/MDE/e/main.js
@@ -39,7 +39,7 @@ function draw() {
 
     noFill()
     stroke(255, 0, 0)
-    ellipse(mouseX, mouseY, radius, radius)
+    //ellipse(mouseX, mouseY, radius, radius)
 }
 
 function windowResized() {
