8f690036c485c2716944c4a54dfc7623e469366c
AminaZ8 <62254828+AminaZ8@users.noreply.github.com>
2020-04-19 12:41:44 +0200

    caricato e rinominato cartella O

---
+++ abc/AZA/O/main.js
@@ -0,0 +1,47 @@
+/**
+ * Template per P5JS
+ * Reference: https://p5js.org/reference/
+ */
+
+
+function setup(){
+	// creiamo un'applicazione della dimensione della finestra
+	createCanvas(windowWidth, windowHeight)
+}
+
+// funzione di loop
+function draw(){
+	background(0)
+	translate(width/2, height/2)
+
+
+	fill(255)
+	noStroke()
+	rectMode(CENTER)
+	ellipse(0,0,500,500)
+
+
+	fill(0)
+	noStroke()
+	rectMode(CENTER)
+	ellipse(0, 0, (mouseX-width/2)*2, (mouseY-height/2)*2, 50, 50)
+}
+
+// -- EVENTI ----------------------------------
+
+function windowResized(){
+	// importante: il canvas deve essere ridimensionato assieme alla finestra
+	resizeCanvas(windowWidth, windowHeight)
+}
+
+
+function keyPressed(){
+	if (key == 's' || key == 'S') {
+    saveCanvas('o', 'png');
+  } else if (key == 'x' || key == 'X'){
+    setup()
+  }
+
+  return false;
+
+}
