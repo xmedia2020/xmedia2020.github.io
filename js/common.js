116526c86ef027c0e7cc5f0f4d87269af99f41b8
Andreas <andreas@ertdfgcvb.xyz>
2020-04-29 11:05:49 +0200

    Aggiornamenti vari alla parte comune
    Modifciato common.js
    Inserito due indici di esempio
    Modifica README.md

--- js/common.js
+++ js/common.js
@@ -1,11 +1,13 @@
 /*
  *  ABC - Menu
+ *  Questo frammento di codice è inserito in tutte le pagine del progetto.
+ *  Assume diverse funzioni:
+ *  1. sovrascrive alcuni eventi di default (touchmove)
+ *  2. inietta la micro navigazione per tornare all'indice
+ *  3. inserisce bottone al commento audio
  */
 
-// disable bounce
-document.ontouchmove = function(e){
-    e.preventDefault()
-}
+// 1. Disabilita "bounce" -----------------------------------------------.--
 document.ontouchmove = function(e){
     e.preventDefault()
 }
@@ -18,15 +20,16 @@ function run() {
     const CSS  = {}
 
     CSS.menu  = ""
-    CSS.menu +="width:5em;"
-    CSS.menu +="height:5em;"
+    CSS.menu +="width:10em;"
+    CSS.menu +="height:2em;"
+    CSS.menu +="padding:0.5em;"
     CSS.menu +="background-color:red;"
-    CSS.menu +="position:absolute;"
+    CSS.menu +="position:fixed;"
     CSS.menu +="right:1em;"
     CSS.menu +="top:1em;"
-    CSS.menu +="text-align:center;"
-    CSS.menu +="line-height:4.75em;"
-    CSS.menu +="border-radius:50%;"
+    // CSS.menu +="text-align:center;"
+    // CSS.menu +="line-height:4.75em;"
+    // CSS.menu +="border-radius:50%;"
 
     CSS.a     = ""
     CSS.a    += "text-decoration:none;"
@@ -36,7 +39,7 @@ function run() {
     menu.style.cssText = CSS.menu
 
     const link = document.createElement("a")
-    link.innerHTML =  "abc"
+    link.innerHTML =  "← index"
     link.href = "../../../index.html"
     link.style.cssText = CSS.a
     link.addEventListener("mouseenter", (e) => {
@@ -46,8 +49,35 @@ function run() {
 
     })
 
-    // inject
+    // 2. Micro navigazione ------------------------------------------------
     const parent = document.body
     menu.appendChild(link)
     parent.insertBefore(menu, document.body.firstChild)
+
+    // 3. Commento audio ---------------------------------------------------
+
+    const AUDIO_FILE = 'commento.mp3'
+
+    const sound = new Audio(AUDIO_FILE)
+
+    sound.addEventListener('canplaythrough', function(e){
+        const play_btn = document.createElement('button')
+        play_btn.id = "play_btn"
+        play_btn.innerHTML = "♫"
+        menu.appendChild(play_btn)
+
+        play_btn.addEventListener('click', function() {
+            if (this.dataset.playing == 'true') {
+                this.dataset.playing = 'false'
+                sound.pause()
+                sound.currentTime = 0
+            } else {
+                this.dataset.playing = 'true'
+                sound.play()
+            }
+        })
+    })
+
+    sound.load()
+
 }
