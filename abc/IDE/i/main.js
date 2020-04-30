228e8d147f9fb5b036f829b39acda0b4cbfabcaf
ilariadellea <ilaria.sagittario@gmail.com>
2020-04-30 14:24:26 +0200

    Update main.js

--- abc/IDE/i/main.js
+++ abc/IDE/i/main.js
@@ -7,18 +7,14 @@ function setup() {
 function draw() {
   background(0)
 
-  let locX = mouseX - height / 2;
-  let locY = mouseY - width / 2;
-
   ambientLight(200);
   pointLight(200,200,200,10);
 
 
   push();
-  translate(d, d, d);
+  translate(d, d+50, d);
   rotateZ(mouseX * -0.01);
   rotateX(frameCount * 0.01);
-  //rotateY(mouseY  * -0.01);
   ambientMaterial(255,255,255);
   box(150, 350, 50, 600);
   pop();
@@ -27,7 +23,6 @@ function draw() {
   translate(d, d-225, d);
   rotateZ(mouseX * -0.01);
   rotateX(frameCount * 0.01);
-  //rotateY(mouseY  * -0.01);
   ambientMaterial(255,255,255);
   torus(50, 20, 50, 600);
   pop();
