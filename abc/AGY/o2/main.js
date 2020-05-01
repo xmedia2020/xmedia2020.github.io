e273392c045d7ca9431a4fbd4053330c512e7df9
Andreas <andreas@ertdfgcvb.xyz>
2020-05-01 12:44:54 +0200

    Update o2

--- abc/AGY/o2/main.js
+++ abc/AGY/o2/main.js
@@ -7,34 +7,44 @@
 function setup(){
 	// creiamo un'applicazione della dimensione della finestra
 	createCanvas(windowWidth, windowHeight)
+	smooth()
 }
 
 // funzione di loop
 function draw(){
 	background(0)
 
-	noFill()
 	stroke(255)
+	noFill()
 	translate(width/2, height/2)
 
 	const scale = min(width, height) / 100
 
-	const cx = map(mouseX, 0, width, -1, 1)
-	const cy = map(mouseY, 0, height, -1, 1)
+	const cx = map(mouseX, 0, width, -1, 1) * scale * 0.02
+	const cy = map(mouseY, 0, height, -1, 1) * scale * 0.02
 
-	const s1 = map(sin(frameCount * 0.030), -1, 1, scale * 20, scale * 30)
-	const s2 = map(sin(frameCount * 0.035), -1, 1, scale * 30, scale * 60)
-	const num = 40
+	const s1 = map(sin(frameCount * 0.009), -1, 1, scale * 5, scale * 25)
+	const num = 180
+	const ang = sin(frameCount * 0.0013) * 0.02
+	const lati = 5//map(sin(frameCount * 0.0011), -1, 1, 3, 12)
 	for (let i=0; i<num; i++){
-		const d = map(i, 0, num-1, s1, s2)
-		const ex = cx * i * scale * 0.5
-		const ey = cy * i * scale * 0.5
-		ellipse(ex, ey, d, d)
+		const r = map(i*i, 0, pow(num-1, 2), s1, s1 * 40)
+		const ex = cx * i * i
+		const ey = cy * i * i
+		poly(ex, ey, r, (num-i) * ang, lati)
 	}
 }
 
-// -- EVENTI ----------------------------------
+function poly(x, y, r, rot, res) {
+	beginShape()
+	for (let i=0; i<res; i++) {
+		const a = TAU / res * i + rot - HALF_PI
+		vertex(x + Math.cos(a) * r, y + Math.sin(a) * r)
+	}
+	endShape(CLOSE)
+}
 
+// -- EVENTI ----------------------------------
 function windowResized(){
 	// importante: il canvas deve essere ridimensionato assieme alla finestra
 	resizeCanvas(windowWidth, windowHeight)
