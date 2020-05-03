36eace92475b1e3efa50d8828527eedeeeeb97ae
lenaccc <63096494+lenaccc@users.noreply.github.com>
2020-05-03 20:50:04 +0200

    aggiunta lettera O

--- abc/OCO/V/main.js
+++ abc/OCO/V/main.js
@@ -1,19 +1,7 @@
-/**
- * Template per P5JS
- * Reference: https://p5js.org/reference/
- */
+
+let heart = 0
 
 
-
-
-
- //console.log(punti[])
- //console.table crea una tabella
- //console.log( "numero di punti" + punti.lenght /2 ) dice quanti sono
-
-
-
-// funzione d’inizio
 function setup() {
    createCanvas( windowWidth, windowHeight, WEBGL );
    angleMode(DEGREES)
@@ -23,46 +11,52 @@ function draw() {
    background(0);
    stroke(255);
    const s = min(width, height)/2;
-  	ambientLight(0);
+   ambientLight(heart);
    //specularMaterial( 200,200,200 )
    //pointLight( 230,230,230, -60,0,90 );
 
-  	let rot = map(mouseX-width/2,0, width, 0,90)
+   let rot = map(mouseX-width/2,0, width, 0,25)
    //camera( 100,100,200, 0,0,0, 0,1,0 );
    translate(0,height/4)
    push()
    rotate((-rot))
    push()
-   rotateX(-30)
+   rotateX(-55)
    rotateY(frameCount*0.5)
    translate(0,-s/2)
-   cone( 50, s );
+   cone( 60, s );
    pop()
    pop()
    stroke(255);
    push()
    rotate((rot))
    push()
-   rotateX(-30)
+   rotateX(-55)
    rotateY(-frameCount*0.5)
    translate(0,-s/2)
-   cone( 50, s );
+   cone( 60, s );
    pop()
    pop()
 }
-	
+   
 
 
 
 // -- EVENTI ----------------------------------
 
 function windowResized(){
-	// importante: il canvas deve essere ridimensionato assieme alla finestra
-	resizeCanvas(windowWidth, windowHeight)
+   // importante: il canvas deve essere ridimensionato assieme alla finestra
+   resizeCanvas(windowWidth, windowHeight)
 }
 
 function keyPressed(){
 
+   if (key == "h"){
+      heart = 255
+
+   } else if (key == "g"){
+      heart = 0
+   }
 }
 
 function keyReleased(){
@@ -70,6 +64,7 @@ function keyReleased(){
 }
 
 function mousePressed(){
+
 
 }
 
